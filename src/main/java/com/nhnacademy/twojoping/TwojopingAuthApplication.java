e43bb402b82004f29528e373294dc3dda17dd8d7
Sauter001 <lsj457@naver.com>
2024-10-21 09:59:33 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/twojoping/TwojopingAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.twojoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwojopingAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwojopingAuthApplication.class, args);
+	}
+
+}
