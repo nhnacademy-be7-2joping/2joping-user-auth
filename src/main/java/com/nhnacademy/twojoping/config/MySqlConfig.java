ddfca38bd12516aa898aadb3b39abd853bc0fd52
Sauter001 <lsj457@naver.com>
2024-11-04 10:07:59 +0900

    chore: change connection pool  to hikari -> dbcp2

--- src/main/java/com/nhnacademy/twojoping/config/MySqlConfig.java
+++ src/main/java/com/nhnacademy/twojoping/config/MySqlConfig.java
@@ -3,11 +3,12 @@ package com.nhnacademy.twojoping.config;
 import com.nhnacademy.twojoping.dto.response.MysqlKeyResponseDto;
 import com.nhnacademy.twojoping.service.KeyManagerService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,11 +18,19 @@ public class MySqlConfig {
     @Bean
     public DataSource dataSource() {
         MysqlKeyResponseDto keyResponseDto = keyManagerService.getDbConnectionInfo();
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl(keyResponseDto.url());
         dataSource.setUsername(keyResponseDto.username());
         dataSource.setPassword(keyResponseDto.password());
+
+        // DBCP2 커넥션 풀 설정
+        dataSource.setInitialSize(5);
+        dataSource.setMaxTotal(20);
+        dataSource.setMaxIdle(10);
+        dataSource.setMinIdle(5);
+        dataSource.setMaxWait(Duration.ofSeconds(10)); // 10 초
+
         return dataSource;
     }
 }
