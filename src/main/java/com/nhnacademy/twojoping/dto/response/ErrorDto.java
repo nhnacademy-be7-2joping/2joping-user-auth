406ebf6226db63abf7ee84e2e6f87c5790654fe7
YeonWooKimm <kyw10987@gmail.com>
2024-11-21 17:55:47 +0900

    fix: errorDto

--- src/main/java/com/nhnacademy/twojoping/dto/response/ErrorDto.java
+++ src/main/java/com/nhnacademy/twojoping/dto/response/ErrorDto.java
@@ -2,5 +2,5 @@ package com.nhnacademy.twojoping.dto.response;
 
 import org.springframework.http.HttpStatus;
 
-public record ErrorDto(HttpStatus status, String errorCode, String message) {
+public record ErrorDto(int status, String errorCode, String message) {
 }
