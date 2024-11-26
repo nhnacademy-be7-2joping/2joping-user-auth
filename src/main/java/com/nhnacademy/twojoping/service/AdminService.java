17e00d1ae9ac610776643e2ce1a69ca5ac49d153
choeunlee <choeun1500@gmail.com>
2024-11-26 10:52:00 +0900

    chore: add log for test

--- src/main/java/com/nhnacademy/twojoping/service/AdminService.java
+++ src/main/java/com/nhnacademy/twojoping/service/AdminService.java
@@ -4,14 +4,17 @@ import com.nhnacademy.twojoping.exception.MemberNotFoundException;
 import com.nhnacademy.twojoping.model.Admin;
 import com.nhnacademy.twojoping.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AdminService {
     private final AdminRepository adminRepository;
 
     public Admin getAdmin(String id) {
+        log.info("for Test");
         return adminRepository.findById(id).orElseThrow(() -> new MemberNotFoundException(id));
     }
 }
